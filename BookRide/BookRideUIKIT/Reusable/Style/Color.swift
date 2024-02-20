3d8c72b54a39b5adcca713b4ab3e49c39a127b49
Zahid Hasan <megamind.d.evilll@gmail.com>
2024-02-20 11:33:52 +0600

    Added App Color Pallete

--- BookRide/BookRideUIKIT/Reusable/Style/Color.swift
+++ BookRide/BookRideUIKIT/Reusable/Style/Color.swift
@@ -9,45 +9,11 @@ import UIKit
 
 public struct Color{
     //MARK: - Properties
-    public static let background = UIColor(0x121424)
+    public static let background = UIColor(0x654b38)
+    public static let Umber = UIColor(0x6B5B51)
+    public static let TaupeGray = UIColor(0x9E9A9A)
+    public static let EggShell = UIColor(0xEEECDA)
+    public static let SeaSalt = UIColor(0xFAFBFA)
 }
 
-extension UIColor{
-    
-    //MARK: - Methods
-    /// Hex sRGB color initializer.
-    ///
-    /// - parameter hex: Pass in a sRGB color integer using hex notation, i.e. 0xFFFFFF. Make sure to only include 6 hex digits.
-    ///
-    /// - returns: Initialized opaque UIColor, i.e. alpha is set to 1.0.
-    public convenience init(_ hex: Int) {
-        assert (
-            0...0xFFFFFF ~= hex,
-            "UIColor+Hex: Hex value given to UIColor initializer should only include RGB values, i.e. the hex value should have six digits."
-        )
-        
-        let red = (hex & 0xFFFFFF) >> 16
-        let green = (hex & 0x00FF00) >> 8
-        let blue = (hex & 0x0000FF)
-        self.init(red: red, green: green, blue: blue)
-    }
-    
-    /// RGB integer color initializer.
-    ///
-    /// - parameter red:   Red component as integer. In iOS 9 or below, this value should be between 0 and 255. iOS 10
-    ///                    and above uses an extended color space to support wide color.
-    /// - parameter green: Green component as integer. In iOS 9 or below, this value should be between 0 and 255. iOS 10
-    ///                    and above uses an extended color space to support wide color.
-    /// - parameter blue:  Blue component as integer. In iOS 9 or below, this value should be between 0 and 255. iOS 10
-    ///                    and above uses an extended color space to support wide color.
-    ///
-    /// - returns: Initialized opaque UIColor, i.e. alpha is set to 1.0.
-    public convenience init(red: Int, green: Int, blue: Int) {
-        self.init(
-            red: CGFloat(red) / 255.0,
-            green: CGFloat(green) / 255.0,
-            blue: CGFloat(blue) / 255.0,
-            alpha:  1.0
-        )
-    }
-}
+
